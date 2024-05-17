bcd61e24a48f2fe78332b35f8cc67e68bbf1f245
Quinta11 <adrianq1202@gmail.com>
2024-05-17 15:59:28 -0400

    Added subroutines, program runs indefinitely until user types exit command

--- main.pl
+++ main.pl
@@ -5,45 +5,89 @@ use warnings;
 ################################################################################
 
 # Variables pertaining to file I/O
-my $filename = "./hurdat2-natl-latest.txt"; # File to read
-my $encoding = ":encoding(UTF-8)";          # UTF-8 encoding for reading file
-my $handle   = undef;                       # Assigned value on successful open for future referencing
+my $filename = "./hurdat2-natl-latest.txt";         # File to read
+my $encoding = ":encoding(UTF-8)";                  # UTF-8 encoding for reading file
+my $handle   = undef;                               # Assigned value on successful open for future referencing
 
-# Variables to be used in the program
-my @validTypes    = ("TS", "SS", "HU");                 # Valid storm classifications
-my @validTimes    = ("0000", "0600", "1200", "1800");   # Valid fix times
-my $substringYear = "2005";                             # Year to check for ACE total
-my $ACE           = 0;                                  # Accumulated Cyclone Energy
+# Variables to be used as constants
+my @validTypes  = ("TS", "SS", "HU");               # Valid storm classifications
+my @validTimes  = ("0000", "0600", "1200", "1800"); # Valid fix times
+
+# Variables to be adjusted by the user
+my $substringYear = undef;                          # Year to check for ACE total
+
+# Variables to hold acquired values
+my @lines = undef;                                  # Array to hold all lines from file
+my $ACE = 0;                                        # Accumulated Cyclone Energy
 
 ################################################################################
 
-# Open file for reading
-open($handle, "< $encoding", $filename)
-    || die "$0: can't open $filename for reading: $!";
+# Initial print statements (not looped)
+print "Thank you for using my HURDAT2 ACE calculator!\n";
+print "Current functionalities include:\n- Calculating the ACE for a certain year\nCurrent basins supports:\n- North Atlantic (NATL)\n\n";
 
-# Read all lines from file
-my @lines = readline($handle);
+# Home, runs indefinitely until user exits program
+while(1) {
+    print "Please select the year you wish to calculate the ACE total for: ";
+    $substringYear = userInput();
 
-for my $line (@lines) {
-    my @elements = split(',', $line);
+    @lines = readFile($filename, $encoding, $handle);   # Read HURDAT2 text file by calling subroutine
 
-    ### CHECK ACE TOTAL FOR SPECIFIED YEAR
-    # If statement runs if substring is found at the beginning of the first element of each array line
-    if(rindex($elements[0], $substringYear, 0) != -1){
-        # Check if the fix time and classification type are valid for calculation
-        if((grep{$elements[1] =~ /$_/} @validTimes) and (grep {$elements[3] =~ /$_/} @validTypes)) {
-            $ACE += (($elements[6] * $elements[6])/10000);
+    for my $line (@lines) {
+        my @elements = split(',', $line);
+
+        ### CHECK ACE TOTAL FOR SPECIFIED YEAR
+        # If statement runs if substring is found at the beginning of the first element of each array line
+        if(rindex($elements[0], $substringYear, 0) != -1){
+            # Check if the fix time and classification type are valid for calculation
+            if((grep{$elements[1] =~ /$_/} @validTimes) and (grep {$elements[3] =~ /$_/} @validTypes)) {
+                $ACE += calculateACE($elements[6]); # Call subroutine to calculate ACE
+            }
         }
+
+        ### CHECK ACE TOTAL FOR SPECIFIED STORM
+        # To do...
     }
 
-    ### CHECK ACE TOTAL FOR SPECIFIED STORM
-    # To do...
+    # Print ACE total for specified year
+    print "$ACE\n";
+    $ACE = 0;
 }
 
-# Print ACE total for specified year
-print "$ACE\n";
+################################################################################
 
-# Close file
-close($handle)
-    || die "$0: can't close $filename: $!";
+# Subroutine to read file
+sub readFile {
+    my $filename = shift;
+    my $encoding = shift;
+    my $handle   = shift;
+
+    open($handle, "< $encoding", $filename)                 # Open file
+        || die "$0: can't open $filename for reading: $!";
+
+    my @lines = readline($handle);                          # Copy file contents to array
+
+    close($handle)                                          # Close file
+        || die "$0: can't close $filename: $!";
+
+    return @lines;
+}
+
+# Subroutine to get user input
+sub userInput {
+    chomp(my $userInput = <STDIN>); # Read input from user
+
+    if ($userInput eq "exit") {     # Exit the program if user inputs "exit"
+        exit;
+    }
+
+    return $userInput;
+}
+
+# Subroutine to calculate ACE
+sub calculateACE {
+    my $windSpeed = shift;                      # Shift uses firstmost argument passed to subroutine (ie. elements[6])
+
+    return (($windSpeed * $windSpeed)/10000);
+}
 
